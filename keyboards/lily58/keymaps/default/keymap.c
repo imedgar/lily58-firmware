1118384d36cb7c59b25839552f37768d7e625aa2
imedgar <ed.ga.ru90@gmail.com>
2025-07-22 00:15:43 +0300

    custom logo

--- keyboards/lily58/keymaps/default/keymap.c
+++ keyboards/lily58/keymaps/default/keymap.c
@@ -132,7 +132,9 @@ bool oled_task_user(void) {
     //oled_write_ln(read_host_led_state(), false);
     //oled_write_ln(read_timelog(), false);
   } else {
-    oled_write(read_logo(), false);
+    //oled_write(read_logo(), false); // original logo
+    oled_write_raw_P(read_logo(), 512); // custom logo
+
   }
     return false;
 }
